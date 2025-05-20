06a6643f2ff7eacde80d97d1b427f7360757164a
AnjeshxShrestha <anjeshshrestha039@gmail.com>
2025-05-20 21:19:41 +0545

    Create todo_storage.py

---
+++ todo_storage.py
@@ -0,0 +1,14 @@
+import os
+import json
+
+TODO_FILE = "todos.json"
+
+def load_todos():
+    if not os.path.exists(TODO_FILE):
+        return []
+    with open(TODO_FILE, "r") as file:
+        return json.load(file)
+
+def save_todos(todos):
+    with open(TODO_FILE, "w") as file:
+        json.dump(todos, file, indent=4)
